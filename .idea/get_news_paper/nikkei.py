f6e968431f061515c0428eaf457b241991823fd3
Hoshino Eiko <eikostarsmile@yahoo.co.jp>
2018-03-04 17:18:07 +0900

    yyyymmdd changed

--- .idea/get_news_paper/nikkei.py
+++ .idea/get_news_paper/nikkei.py
@@ -35,13 +35,62 @@ def login(url,ID,PASSWORD):
     soup = BeautifulSoup(html, "lxml")
     #print(soup)
 
-def get_text():
-    driver.get('https://www.nikkei.com/economy/economic/')
-    articles=driver.find_elements_by_xpath("//a[contains(@href,'article')]")
-
+def collect_articles(articles):
+    urls = []
+    i=0
     for article in articles:
         url = article.get_attribute('href')
-        print(url)
+        if url not in urls:
+            urls.append(url)
+            i=1+i
+            print(str(i),url)
+        if i==20:
+            break
+    return urls
+
+
+def get_text():
+    driver.get('https://www.nikkei.com/economy/economic/')
+    size_articles=driver.find_element_by_class_name("m-pageNation").text
+    size_articles=int(size_articles[0:size_articles.find('中')-1])
+    print(size_articles)
+    articles=driver.find_elements_by_xpath("//a[contains(@href,'article')]")
+
+    urls =collect_articles(articles)
+
+    driver.get(urls[1])
+    html = driver.page_source.encode('utf-8')  # more sophisticated methods may be available
+    soup = BeautifulSoup(html, "lxml")
+    title = soup.find("head").find("title").text
+    date = driver.find_element_by_class_name("cmnc-publish").text.replace(' ','@')
+    date_str_list=list(date)
+    print(date)
+    year=date[0:4]
+    month=date[5:date.rfind('/')]
+    date=date[date.rfind('/')+1:date.find('@')]
+    hour=date[date.find('@'):-1]
+    minitus=date[date.find(':')+1:-1]
+
+    print(year+month+date+hour+minitus)
+    print(hour)
+    print(minitus)
+
+    month_changed=False
+    for i in range(len(date_str_list)):
+        if date_str_list[i]=='/':
+            date_str_list[i]='月'
+            month_changed=True
+        elif date_str_list[i]=='/' and month_changed==True:
+            date_str_list[i]='日'
+
+
+
+
+    print(date_str_list)
+    print(title)
+
+    tags=driver.find_elements_by_xpath("//dd[contains(@class,'cmnc-tag')]")
+
 
 
 class nikkei():
